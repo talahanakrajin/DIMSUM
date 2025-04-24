9ae3f777ec6aed968c9bfa0d20e06a021fb7345b
Barrizzz <barripratama.bp@gmail.com>
2025-04-24 18:59:44 +0700

    testing

--- Main.java
+++ Main.java
@@ -1,6 +1,27 @@
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("This is a simple Java program.");
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(500, "T001");
+        treeMap.put(510, "T002");
+        treeMap.put(530, "T004");
+        treeMap.put(540, "T005");
+        treeMap.put(600, "T006");
+
+        // Display the TreeMap
+        System.out.println("TreeMap: " + treeMap);
+
+        Methods methods = new Methods(); 
+
+        methods.addEntry(treeMap, 520, "T003");
+        System.out.println("After adding T003: " + treeMap);
+        methods.addEntry(treeMap, 530, "T007");
+        System.out.println("After adding T007: " + treeMap);
+
+        methods.removeEntry(treeMap, 510);
+        System.out.println("After removing T002: " + treeMap);
+        methods.removeEntry(treeMap, 600);
+        System.out.println("After removing T006: " + treeMap);
     }
 }
